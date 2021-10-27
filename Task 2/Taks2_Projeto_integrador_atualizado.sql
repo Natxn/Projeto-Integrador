4fca6460be03ec237587bbc2631b2d0ef756697a
Caroline Calixto <carolinecalixto.santos@gmail.com>
2021-10-27 13:41:32 -0300

    Atualização após mentoria

--- Task 2/Taks2_Projeto_integrador_atualizado.sql
+++ Task 2/Taks2_Projeto_integrador_atualizado.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `tb_usuario` (
+	`id_usuario` bigint NOT NULL AUTO_INCREMENT,
 	`email` varchar(100) NOT NULL,
 	`nome` varchar(100) NOT NULL,
 	`senha` varchar(8) NOT NULL,
-	PRIMARY KEY (`email`)
+	PRIMARY KEY (`id_usuario`)
 );
 
 CREATE TABLE `tb_postagem` (
@@ -10,21 +11,21 @@ CREATE TABLE `tb_postagem` (
 	`titulo` varchar(255) NOT NULL,
 	`texto` varchar(8000) NOT NULL,
 	`curtida` int NOT NULL,
-	`imagem` longblob NOT NULL,
-	`fk_email` varchar(100) NOT NULL,
+	`imagem` varchar(255) NOT NULL,
+	`fk_id_usuario` bigint NOT NULL,
 	`fk_id_tema` bigint NOT NULL,
 	PRIMARY KEY (`id_postagem`)
 );
 
 CREATE TABLE `tb_tema` (
 	`id_tema` bigint NOT NULL AUTO_INCREMENT,
-	`categoria` varchar(255) NOT NULL,
-	`salvar` BINARY NOT NULL,
-	`alta_dia` BOOLEAN NOT NULL,
+	`nome_categoria` varchar(255) NOT NULL,
+	`descricao_categoria` varchar(255) NOT NULL,
+	`postagens_salvas` BOOLEAN NOT NULL,
 	PRIMARY KEY (`id_tema`)
 );
 
-ALTER TABLE `tb_postagem` ADD CONSTRAINT `tb_postagem_fk0` FOREIGN KEY (`fk_email`) REFERENCES `tb_usuario`(`email`);
+ALTER TABLE `tb_postagem` ADD CONSTRAINT `tb_postagem_fk0` FOREIGN KEY (`fk_id_usuario`) REFERENCES `tb_usuario`(`id_usuario`);
 
 ALTER TABLE `tb_postagem` ADD CONSTRAINT `tb_postagem_fk1` FOREIGN KEY (`fk_id_tema`) REFERENCES `tb_tema`(`id_tema`);
 
